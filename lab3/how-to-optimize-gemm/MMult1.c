20abc157552a7a161699b042eeea5514ab4ae5fd
xrlexpert <hiroxrl@outlook.com>
2023-09-20 16:00:58 +0800

    update

--- lab3/how-to-optimize-gemm/MMult1.c
+++ lab3/how-to-optimize-gemm/MMult1.c
@@ -1,5 +1,5 @@
 /* Create macros so that the matrices are stored in column-major order */
-
+#include "f77blas.h"
 #define A(i,j) a[ (j)*lda + (i) ]
 #define B(i,j) b[ (j)*ldb + (i) ]
 #define C(i,j) c[ (j)*ldc + (i) ]
@@ -8,20 +8,25 @@
 
 void AddDot( int, double *, int, double *, double * );
 
-void MY_MMult( int m, int n, int k, double *a, int lda, 
-                                    double *b, int ldb,
-                                    double *c, int ldc )
+void MY_MMult( int m, int n, int k, double *A, int lda, 
+                                    double *B, int ldb,
+                                    double *C, int ldc )
 {
-  int i, j;
+  char ta = 'N';
+  char tb = 'N';
+  double alpha = 1.2;
+  double beta = 0.001;
+  dgemm_(&ta, &tb, &m, &n, &k, &alpha, A, &m, B, &k, &beta, C, &m);
+  // int i, j;
 
-  for ( j=0; j<n; j+=1 ){        /* Loop over the columns of C */
-    for ( i=0; i<m; i+=1 ){        /* Loop over the rows of C */
-      /* Update the C( i,j ) with the inner product of the ith row of A
-	 and the jth column of B */
+  // for ( j=0; j<n; j+=1 ){        /* Loop over the columns of C */
+  //   for ( i=0; i<m; i+=1 ){        /* Loop over the rows of C */
+  //     /* Update the C( i,j ) with the inner product of the ith row of A
+	//  and the jth column of B */
 
-      AddDot( k, &A( i,0 ), lda, &B( 0,j ), &C( i,j ) );
-    }
-  }
+  //     AddDot( k, &A( i,0 ), lda, &B( 0,j ), &C( i,j ) );
+  //   }
+  // }
 }
 
 
@@ -29,16 +34,16 @@ void MY_MMult( int m, int n, int k, double *a, int lda,
 
 #define X(i) x[ (i)*incx ]
 
-void AddDot( int k, double *x, int incx,  double *y, double *gamma )
-{
-  /* compute gamma := x' * y + gamma with vectors x and y of length n.
+// void AddDot( int k, double *x, int incx,  double *y, double *gamma )
+// {
+//   /* compute gamma := x' * y + gamma with vectors x and y of length n.
 
-     Here x starts at location x with increment (stride) incx and y starts at location y and has (implicit) stride of 1.
-  */
+//      Here x starts at location x with increment (stride) incx and y starts at location y and has (implicit) stride of 1.
+//   */
  
-  int p;
+//   int p;
 
-  for ( p=0; p<k; p++ ){
-    *gamma += X( p ) * y[ p ];     
-  }
-}
+//   for ( p=0; p<k; p++ ){
+//     *gamma += X( p ) * y[ p ];     
+//   }
+// }
